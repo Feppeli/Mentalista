e70ca071bdde46c36dfee0c65a76cde036b70dbb
Felipe Teixeira <80359142+Feppeli@users.noreply.github.com>
2021-09-23 12:15:56 -0300

    Update mentalista.js

--- mentalista.js
+++ mentalista.js
@@ -1,11 +1,11 @@
-var numeroSecreto = parseInt(Math.random() * 11)
+var numeroSecreto = parseInt(Math.random() * 11)  //Variável que gera os números aleatóriamente
 
-function Chutar() {
-    var resultado = document.getElementById("resultado")
+function Chutar() { //função ligada ao botão chutar
+    var resultado = document.getElementById("resultado") // buscando as informações do "resultado" no HTML
     var chute = parseInt(document.getElementById("valor").value);
 
-    if(chute == numeroSecreto) {
-       resultado.innerHTML = "Você ACERTOU!!!!!"
+    if(chute == numeroSecreto) { // criando a condicionais para a função cutar
+       resultado.innerHTML = "Você ACERTOU!!!!!" // imprimindo na tela pelo HTML
     
     }else if(chute > 10 || chute < 0) {
         resultado.innerHTML = "Você deve digitar um valor entre 0 e 10"
